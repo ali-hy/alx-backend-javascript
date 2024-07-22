752084a207aa6fd544d827c43988eba573e397df
ali-hy <ali.haitham.ymy@gmail.com>
2024-07-22 23:04:06 +0400

    13

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+  const allEmployees = report.allEmployees;
+  let employees = [];
+
+  for (const department of Object.values(allEmployees)) {
+    employees = employees.concat(department);
+  }
+
+  return employeesSymbol.iterator;
+}
