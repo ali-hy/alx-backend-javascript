3428a88ff8856c177e72d6a9c02b38e91ebbfbfa
ali-hy <ali.haitham.ymy@gmail.com>
2024-07-25 01:09:44 +0400

    push 8-try.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+// Create and export the handleProfileSignup function
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  // Call the imported functions and handle their promises
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  // Return an array with the status and value of each promise once they are all settled
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
     .then((results) => results.map((result) => ({
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
