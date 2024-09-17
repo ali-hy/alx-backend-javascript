cd914a13ff3101387a4464d652062ebd02dc3cc2
ali-hy <ali.haitham.ymy@gmail.com>
2024-09-18 00:46:28 +0300

    2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,24 @@
+module.exports = function countStudents(path) {
+  try {
+    const data = fs.readFileSync
+    (path, { encoding: 'utf8' }).split('\n');
+    let students = data.slice(1, data.length - 1);
+    let fields = {};
+    for (const student of students) {
+      if (student in fields) {
+        fields[student] += 1;
+      } else {
+        fields[student] = 1;
+      }
+    }
+    console.log(`Number of students: ${students.length}`);
+
+    for (const field in fields) {
+      if (field) {
+        console.log(`Number of students in ${field}: ${fields[field]}. List: ${students.filter((student) => student === field).join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
