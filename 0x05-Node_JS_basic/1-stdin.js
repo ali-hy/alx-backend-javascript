80ca34a47db6878d64f89c2619e09c7c0070107c
ali-hy <ali.haitham.ymy@gmail.com>
2024-09-18 00:41:46 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString();
+process.stdin.on('readable', (data) => {
+  const chunk = process.stdin.read();
 
-  if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
-  process.exit();
 });
 
 process.on('exit', () => {
