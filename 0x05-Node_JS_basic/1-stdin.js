374b4deddcf52a00ebc3361d2f6bf2accf5f68b6
ali-hy <ali.haitham.ymy@gmail.com>
2024-09-18 00:34:59 +0300

    1-stdin.js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+const displayMessage = require('./0-console');
+
+displayMessage('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  displayMessage(`Your name is: ${name}`);
+  process.exit();
+});
+
+process.on('exit', () => {
+  displayMessage('This important software is now closing');
+});
