7d233ddcc873656031f7bd7bf4c1c2ec420668b4
ali-hy <ali.haitham.ymy@gmail.com>
2024-09-18 00:42:47 +0300

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', (data) => {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
 
   if (name) {
